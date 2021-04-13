e8f1799e74d9fd347d10d17fa2f12bbd816df1c4
stedyyulius <stedyyulius@gmail.com>
2021-04-13 12:37:57 +0700

    add cors

--- api/index.js
+++ api/index.js
@@ -6,8 +6,11 @@ const screenshotmachine = require('screenshotmachine');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 const port = 3002
 
@@ -28,20 +31,20 @@ app.get('/api/screenshot', async (req, res) => {
 
         res.setHeader('Content-Type', 'text/html')
         res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-      
+
         const customerKey = process.env.API_KEY;
 
         secretPhrase = ''; //leave secret phrase empty, if not needed
         options = {
-        //mandatory parameter
-        url : req.query.url,
-        // all next parameters are optional, see our website screenshot API guide for more details
-        dimension : '1366xfull', // or "1366xfull" for full length screenshot
-        device : 'desktop',
-        format: 'png',
-        cacheLimit: '0',
-        delay: '200',
-        zoom: '100'
+            //mandatory parameter
+            url: req.query.url,
+            // all next parameters are optional, see our website screenshot API guide for more details
+            dimension: '1366xfull', // or "1366xfull" for full length screenshot
+            device: 'desktop',
+            format: 'png',
+            cacheLimit: '0',
+            delay: '200',
+            zoom: '100'
         }
 
         const apiUrl = screenshotmachine.generateScreenshotApiUrl(customerKey, secretPhrase, options);
@@ -53,15 +56,15 @@ app.get('/api/screenshot', async (req, res) => {
 
         console.log(output)
 
-        screenshotmachine.readScreenshot(apiUrl).pipe(fs.createWriteStream(output).on('close', function() {
-        console.log('Screenshot saved as ' + output);
-        
-        const base64 = fs.readFileSync(path.join(os.tmpdir(), 'output.png'), 'base64')
+        screenshotmachine.readScreenshot(apiUrl).pipe(fs.createWriteStream(output).on('close', function () {
+            console.log('Screenshot saved as ' + output);
 
-        res.send(base64);
-    }));
+            const base64 = fs.readFileSync(path.join(os.tmpdir(), 'output.png'), 'base64')
 
-    } catch(error) {
+            res.send(base64);
+        }));
+
+    } catch (error) {
         res.status(400).send(error.stderr)
     }
 
